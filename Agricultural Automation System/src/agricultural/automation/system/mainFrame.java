a30204e5d08d6761d8123048ae6a10cd67426ac3
abdo <abdoabdo2810575@gmail.com>
2024-02-05 15:03:55 +0200

    tets ignor

--- Agricultural Automation System/src/agricultural/automation/system/mainFrame.java
+++ Agricultural Automation System/src/agricultural/automation/system/mainFrame.java
@@ -44,7 +44,7 @@ public class MainFrame extends javax.swing.JFrame {
         
     }
 
-    //....................,l
+    //............a........ss,lس
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated
